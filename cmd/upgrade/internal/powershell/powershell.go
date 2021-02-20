d11bfe11e49486f6ffb68670bec656452e9eb49e
Arvind Iyengar <arvind.iyengar@rancher.com>
2021-02-19 19:29:17 -0800

    Add wins cli upgrade

--- cmd/upgrade/internal/powershell/powershell.go
+++ cmd/upgrade/internal/powershell/powershell.go
@@ -1,6 +1,7 @@
 package powershell
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os/exec"
@@ -8,6 +9,9 @@ import (
 )
 
 // Sourced from https://github.com/flannel-io/flannel/blob/d31b0dc85a5a15bda5e606acbbbb9f7089441a87/pkg/powershell/powershell.go
+
+//commandWrapper ensures that exceptions are written to stdout and the powershell process exit code is -1
+const commandWrapper = `$ErrorActionPreference="Stop";try { %s } catch { Write-Host $_; os.Exit(-1) }`
 
 // RunCommand executes a given powershell command.
 //
@@ -33,4 +37,27 @@ func RunCommand(command string) ([]byte, error) {
 // When the command throws a powershell exception, RunCommandf will return the exception message as error.
 func RunCommandf(command string, a ...interface{}) ([]byte, error) {
 	return RunCommand(fmt.Sprintf(command, a...))
+}
+
+// RunCommandWithJSONResult executes a given powershell command.
+// The command will be wrapped with ConvertTo-Json.
+//
+// You can Wrap your command with @(<cmd>) to ensure that the returned json is an array
+//
+// When the command throws a powershell exception, RunCommandf will return the exception message as error.
+func RunCommandWithJSONResult(command string, v interface{}) error {
+	wrappedCommand := fmt.Sprintf(commandWrapper, "ConvertTo-Json (%s)")
+	wrappedCommand = fmt.Sprintf(wrappedCommand, command)
+
+	stdout, err := RunCommandf(wrappedCommand)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(stdout, v)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- cmd/upgrade/upgrade.go
+++ cmd/upgrade/upgrade.go
@@ -17,17 +17,32 @@ $ErrorActionPreference = "Stop";
 $newBinPath = "%[1]s"
 $winsArgs = "%[2]s"
 
-$currBinPath = (Get-CimInstance Win32_Service -Filter 'Name = "rancher-wins"').PathName.Split(' ')[0]
+$winsProcess = (Get-CimInstance Win32_Service -Filter 'Name = "rancher-wins"')
 
-Get-Service -Name rancher-wins | Stop-Service;
-
-sc.exe delete rancher-wins
-
-Copy-Item -Recurse -Force -Path $newBinPath -Destination $currBinPath | Out-Null
+if ($winsProcess -ne $null) {
+	$currBinPath = $winsProcess.PathName.Split(' ')[0]
+	if ($currBinPath -ne $newBinPath) {
+		Get-Service -Name rancher-wins | Stop-Service;
+		sc.exe delete rancher-wins
+		Copy-Item -Recurse -Force -Path $newBinPath -Destination $currBinPath | Out-Null
+	}
+} else {
+	$currBinPath = $newBinPath
+}
 
 "$currBinPath srv app run $winsArgs" | iex
 
 Start-Service -Name rancher-wins;
+}`
+
+const restartServicePS1 = `& {
+	$ErrorActionPreference = "Stop";
+
+	$winsProcess = (Get-CimInstance Win32_Service -Filter 'Name = "rancher-wins"')
+	if ($winsProcess -ne $null) {
+		"$winsProcess" | iex
+		Start-Service -Name rancher-wins;
+	}
 }`
 
 var _upgradeFlags = []cli.Flag{
@@ -40,6 +55,10 @@ var _upgradeFlags = []cli.Flag{
 		Name:  "wins-args",
 		Usage: "[optional] Arguments to pass onto wins srv app run",
 		Value: "--register",
+	},
+	cli.BoolFlag{
+		Name:  "debug",
+		Usage: "[optional] whether to print debugging logs from performing the upgrade",
 	},
 }
 
@@ -66,18 +85,26 @@ func _upgradeRequestParser(cliCtx *cli.Context) (err error) {
 }
 
 func _upgradeAction(cliCtx *cli.Context) (err error) {
+	if cliCtx.Bool("debug") {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
 	binPath := cliCtx.String("binary")
 	winsArgs := cliCtx.String("wins-args")
-	out, err := powershell.RunCommand(fmt.Sprintf(upgradePS1Fmt, binPath, winsArgs))
+	out, err := powershell.RunCommandf(upgradePS1Fmt, binPath, winsArgs)
+	if len(out) > 0 {
+		logrus.Debugf("logs from upgrade.ps1 \n%s\nEOF", out)
+	}
 	if err != nil {
 		logrus.Error("upgrade failed, attempting to ensure rancher-wins service is not stopped: %v", err)
-		_, restartServiceErr := powershell.RunCommand("Start-Service -Name rancher-wins")
+		out, restartServiceErr := powershell.RunCommand(restartServicePS1)
+		if len(out) > 0 {
+			logrus.Debugf("logs from upgrade.ps1 \n%s\nEOF", out)
+		}
 		if err != nil {
 			logrus.Errorf("unable to restart rancher-wins service: %v", restartServiceErr)
 		}
 		return err
 	}
 	logrus.Info("upgrade succeeded")
-	logrus.Info(out)
 	return nil
 }
